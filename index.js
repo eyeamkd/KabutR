456125952043fca1887169e6da418dcb31260477
Kunal Dubey <kunaldubey2297@gmail.com>
2020-02-13 13:02:02 +0530

    Added Figlet Feature and Error Message

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ let nodemailer = require("nodemailer");
 let CronJob = require("cron").CronJob;
 let mailCreds = require("./mailCreds.json");
 let botToken = require("./botToken.json");
-let telegram = require("telegram-bot-api");
+let telegram = require("telegram-bot-api"); 
+const figlet = require("figlet");
 
 let EVENING_MAIL_CONFIRMATION = "";
-let MORNING_MAIL_CONFIRMATION = ""; 
-let TO_DO = '';
-let DAILY_REPORT = '';
-let d= new Date(); 
+let MORNING_MAIL_CONFIRMATION = "";
+let TO_DO = "";
+let DAILY_REPORT = "";
+let d = new Date();
 let api = new telegram({
   token: botToken.token,
   updates: {
@@ -25,7 +26,7 @@ let mailerConfig = {
     user: mailCreds.email,
     pass: mailCreds.password
   }
-}; 
+};
 
 let transporter = nodemailer.createTransport(mailerConfig);
 
@@ -35,65 +36,67 @@ let morningMailOptions = {
   cc: mailCreds.trainerMail,
   subject: `TO DO | ${d.getDate} + "." + ${d.getMonth} + ${d.getFullYear}`,
   text: ""
-}; 
-
+};
 
 let eveningMailOptions = {
-    from: mailerConfig.auth.user,
-    to: mailCreds.mentorMail,
-    cc: mailCreds.trainerMail,
-    subject: `TO DO | ${d.getDate} + "." + ${d.getMonth} + ${d.getFullYear}`,
-    text: ""
-  };
+  from: mailerConfig.auth.user,
+  to: mailCreds.mentorMail,
+  cc: mailCreds.trainerMail,
+  subject: `TO DO | ${d.getDate} + "." + ${d.getMonth} + ${d.getFullYear}`,
+  text: ""
+};
 
-const morningMail = () => { 
-   if(TO_DO!==null){  
+const morningMail = () => {
+  if (TO_DO !== null) {
     transporter.sendMail(morningMailOptions, function(error) {
-        if (error) {
-          console.log("error:", error);
-        } else {
-          console.log("good"); 
-          MORNING_MAIL_CONFIRMATION = "Wah Dubey ji Wah! 😎 Sent Successfully"; 
-          api.sendMessage({ 
-              chat_id : mailCreds.chatId, 
-              text : MORNING_MAIL_CONFIRMATION
-          })
-        }
-      }); 
-   } 
-   else { 
-    api.sendMessage({ 
-        chat_id : mailCreds.chatId, 
-        text : "What Kunal, you didn't send me the Tasks 😑😑. KabutR couldn't send the mail"
-    })
-   }
-}; 
+      if (error) {
+        console.log("error:", error);
+      } else {
+        console.log("good");
+        MORNING_MAIL_CONFIRMATION = "Wah Dubey ji Wah! 😎 Sent Successfully";
+        api.sendMessage({
+          chat_id: mailCreds.chatId,
+          text: MORNING_MAIL_CONFIRMATION
+        });
+      }
+    });
+  } else {
+    api.sendMessage({
+      chat_id: mailCreds.chatId,
+      text:
+        "What Kunal, you didn't send me the Tasks 😑😑. KabutR couldn't send the mail"
+    });
+  }
+  TO_DO = null;
+};
 
-const eveningMail = () => { 
-    if(DAILY_REPORT!==null){  
-     transporter.sendMail(eveningMailOptions, function(error) {
-         if (error) {
-           console.log("error:", error);
-         } else {
-           console.log("good"); 
-           EVENING_MAIL_CONFIRMATION = "Great Job, I've sent the mail to your mentors! You can happily Enjoy now 🎉"; 
-           api.sendMessage({ 
-               chat_id : mailCreds.chatId, 
-               text : EVENING_MAIL_CONFIRMATION
-           })
-         }
-       }); 
-    } 
-    else { 
-     api.sendMessage({ 
-         chat_id : mailCreds.chatId, 
-         text : "What Kunal, you didn't send me the Daily Report 😑😑. KabutR couldn't send the mail"
-     })
-    }
- };
+const eveningMail = () => {
+  if (DAILY_REPORT !== null) {
+    transporter.sendMail(eveningMailOptions, function(error) {
+      if (error) {
+        console.log("error:", error);
+      } else {
+        console.log("good");
+        EVENING_MAIL_CONFIRMATION =
+          "Great Job, I've sent the mail to your mentors! You can happily Enjoy now 🎉";
+        api.sendMessage({
+          chat_id: mailCreds.chatId,
+          text: EVENING_MAIL_CONFIRMATION
+        });
+      }
+    });
+  } else {
+    api.sendMessage({
+      chat_id: mailCreds.chatId,
+      text:
+        "What Kunal, you didn't send me the Daily Report 😑😑. KabutR couldn't send the mail"
+    });
+  }
+  DAILY_REPORT = null;
+};
 
-const morningMailReminder = () => { 
-    console.log(mailCreds.chatId);
+const morningMailReminder = () => {
+  console.log(mailCreds.chatId);
   api
     .sendMessage({
       chat_id: mailCreds.chatId,
@@ -101,54 +104,89 @@ const morningMailReminder = () => {
     })
     .then(() => {
       api.on("message", function(message) {
-        if(message.text[0] === '$'){ 
-            messageText = message.text.toString();
-            console.log(messageText);
-            TO_DO = messageText.substring(1,messageText.length) +"\n"+ "\n"+ "\n" + "Regards, \n" + "Kunal :)"
-            console.log("Your To do is : ", TO_DO); 
-            morningMailOptions.text = TO_DO;   
-            morningMailOptions.subject = "TO DO | " + d.getDate() + "." + String(d.getMonth() + 1 ) + "." +  d.getFullYear()
-        }  
-      }); 
-    })  
-    .catch(err => { 
-        console.log("Error in saving message", err);
+        if (message.text[0] === "$") {
+          messageText = message.text.toString();
+          console.log(messageText);
+          TO_DO =
+            messageText.substring(1, messageText.length) +
+            "\nRegards, \n" +
+            "Kunal :)";
+          console.log("Your To do is : ", TO_DO);
+          morningMailOptions.text = TO_DO;
+          morningMailOptions.subject =
+            "TO DO | " +
+            d.getDate() +
+            "." +
+            String(d.getMonth() + 1) +
+            "." +
+            d.getFullYear();
+        }
+      });
     })
+    .catch(err => {
+      console.log("Error in sending daily Report ", err);
+      api.sendMessage({
+        chat_id: mailCreds.chatId,
+        text:
+          "Sorry Kunal 🥺 . An Error occured due to which i couldn't send the mail"
+      });
+    });
 };
 
-const eveningMailReminder = () => { 
-    console.log(mailCreds.chatId);
+const eveningMailReminder = () => {
+  console.log(mailCreds.chatId);
   api
     .sendMessage({
       chat_id: mailCreds.chatId,
-      text: "Hey Kunal! 🎈"+"\n" +"Hope you had a great day at Office Today \n"+" What tasks have you done today ?"
+      text:
+        "Hey Kunal! 🎈" +
+        "\n" +
+        "Hope you had a great day at Office Today \n" +
+        " What tasks have you done today ?"
     })
     .then(() => {
       api.on("message", function(message) {
-        if(message.text[0] === '#'){ 
-            messageText = message.text.toString();
-            console.log(messageText);
-            DAILY_REPORT = messageText.substring(1,messageText.length)  + "\n"+ "\n"+ "\n" + "Regards, \n" + "Kunal :)"
-            console.log("Your To do is : ", DAILY_REPORT); 
-            eveningMailOptions.text = DAILY_REPORT; 
-            eveningMailOptions.subject = "DAILY REPORT | " + d.getDate() + "." + String(d.getMonth() + 1 )+ "." + d.getFullYear()
-        }  
+        if (message.text[0] === "#") {
+          messageText = message.text.toString();
+          console.log(messageText);
+          DAILY_REPORT =
+            messageText.substring(1, messageText.length) +
+            "\nRegards, \n" +
+            "Kunal :)";
+          console.log("Your Daily Report is : ", DAILY_REPORT);
+          eveningMailOptions.text = DAILY_REPORT;
+          eveningMailOptions.subject =
+            "DAILY REPORT | " +
+            d.getDate() +
+            "." +
+            String(d.getMonth() + 1) +
+            "." +
+            d.getFullYear();
+        }
+      });
+    })
+    .catch(err => {
+      console.log("Error in sending daily Report ", err);
+      api.sendMessage({
+        chat_id: mailCreds.chatId,
+        text:
+          "Sorry Kunal 🥺 . An Error occured due to which i couldn't send the mail"
       });
     });
-}; 
+};
 
 let morningMailjobReminder = new CronJob(
-    "00 09 16 * * 1-6",
-    function() {
-      morningMailReminder();
-    },
-    null,
-    true,
-    "Asia/Kolkata"
-); 
+  "00 00 9 * * 1-6",
+  function() {
+    morningMailReminder();
+  },
+  null,
+  true,
+  "Asia/Kolkata"
+);
 
 let morningMailjob = new CronJob(
-  "00 10 16 * * 1-6",
+  "00 30 9 * * 1-6",
   function() {
     morningMail();
   },
@@ -156,32 +194,44 @@ let morningMailjob = new CronJob(
   true,
   "Asia/Kolkata"
 );
- 
+
 let eveningMailjobReminder = new CronJob(
-    "00 11 16 * * 1-6",
-    function() {
-      eveningMailReminder();
-    },
-    null,
-    true,
-    "Asia/Kolkata"
+  "00 00 18 * * 1-6",
+  function() {
+    eveningMailReminder();
+  },
+  null,
+  true,
+  "Asia/Kolkata"
 );
 
 let eveningMailjob = new CronJob(
-  "00 12 16 * * 1-6",
+  "00 30 18 * * 1-6",
   function() {
     eveningMail();
   },
   null,
   true,
   "Asia/Kolkata"
-); 
+);
 
-morningMailjob.start(); 
-eveningMailjob.start();  
+figlet.text('KabutR', {
+  font: 'Doh',
+  horizontalLayout: 'default',
+  verticalLayout: 'default'
+}, function(err, data) {
+  if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+  }
+  console.log(data); 
+}); 
 
-morningMailjobReminder.start(); 
-eveningMailjobReminder.start(); 
 
 
+morningMailjob.start();
+eveningMailjob.start();
 
+morningMailjobReminder.start();
+eveningMailjobReminder.start();
